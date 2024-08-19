c1f0448823dee8b42026570ec23c2171c3cdfe31
Sujal Soni <sujalkrsoni@gmail.com>
2024-08-19 15:12:50 +0530

    added

---
+++ ind.js
@@ -0,0 +1,10 @@
+// const { name: myName } = { name: 'Lydia' };
+
+// console.log(name);
+
+function component(){
+    return (function (){
+        return "welcome <h2> Sujal </h2>"  // open in web to see the result 
+    });
+}
+console.log(component()());
---
+++ library/modules/index.js
@@ -0,0 +1,10 @@
+// let title = "FASHION STORE | HOME"
+
+// export function printTitle(){
+//     return title;
+// }
+
+// export function printSubTitle(){
+//     return "Online shopping"
+// }
+
